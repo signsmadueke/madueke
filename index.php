e8dc2bda02090d0136ca30c4384c9e069d4fb7da
Signs Madueke <signsmadueke1@gmail.com>
2020-01-21 20:41:46 +0100

    INIT

---
+++ index.php
@@ -0,0 +1,216 @@
+<?php
+    $title = 'Home';
+    require_once 'inc/header.php';
+?>
+
+
+<section id="bestsellers" class="lg pt-150 pb-150 light full-height hidden-md hidden-sm">
+    <div class="container">
+        <div class="row align-items-center">
+            <div id="title" style="line-height: 36px;color: white;font-size: 17px;letter-spacing: 8px;text-transform: uppercase;font-weight: 500;" class="col-12">
+                <span>Best Selling Books</span>
+            </div>
+            <div class="gallery-item col-md-3 dark shadow gallery-style-6 margin-x2 padding-x2 text-left">
+                <img src="assets/images/books/book2.png" alt="21/40 Nights of Decrees and Your Enemies Will Surrender" class="item-img">
+                <div class="item-title">
+                    <h4 class=""><strong class="">21/40 Nights of Decrees and Your Enemies Will Surrender
+                        </strong></h4>
+                </div>
+                <div class="item-text">
+                    <p>This book is a collection of powerful prayer points divided in two parts. The first part is made up of 21 prayer topics or decrees and the second, 40 decrees. It covers every...</p>
+                </div>
+                <div class="item-button">
+                    <div class="row">
+                        <div class="col-12 mb-10" style="font-size: 10px; letter-spacing: 5px;text-transform: uppercase;font-weight: 500;">Buy Now</div>
+                        <div class="col-6" style="padding-left: 30px; padding-right: 5px;">
+                            <a href="#">
+                                <img src="assets/images/amazon.svg" width="100%" alt="">
+                            </a>
+                        </div>
+                        <div class="col-6" style="padding-left: 5px; padding-right: 30px;">
+                            <a href="#">
+                                <img src="assets/images/kindle.svg" width="100%" alt="">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="gallery-item col-md-3 dark shadow gallery-style-6 margin-x2 padding-x2 text-left">
+                <img src="assets/images/books/book1.png" alt="35 Special Dangerous Decrees" class="item-img">
+                <div class="item-title">
+                    <h4 class=""><strong class="">35 Special Dangerous Decrees</strong></h4>
+                </div>
+                <div class="item-text">
+                    <p class="">35 Special Dangerous Decrees is a collection of targeted prayers addressing 35 most important areas of the Christian life.</p>
+                </div>
+                <div class="item-button">
+                    <div class="row">
+                        <div class="col-12 mb-10" style="font-size: 10px; letter-spacing: 5px;text-transform: uppercase;font-weight: 500;">Buy Now</div>
+                        <div class="col-6" style="padding-left: 30px; padding-right: 5px;">
+                            <a href="#">
+                                <img src="assets/images/amazon.svg" width="100%" alt="">
+                            </a>
+                        </div>
+                        <div class="col-6" style="padding-left: 5px; padding-right: 30px;">
+                            <a href="#">
+                                <img src="assets/images/kindle.svg" width="100%" alt="">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bg-wrap">
+        <div class="bg" style="background-color: transparent;"></div>
+    </div>
+</section>
+<section id="bestsellers" class="md pt-400 pb-0 light full-height hidden-sm hidden-xl hidden-lg">
+    <div class="container">
+        <div class="row align-items-center">
+            <div id="title" style="line-height: 36px;color: white;font-size: 17px;letter-spacing: 8px;text-transform: uppercase;font-weight: 500;" class="col-12">
+                <span>Best Selling Books</span>
+            </div>
+            <div class="gallery-item col-md-3 dark shadow gallery-style-6 margin-x2 padding-x2 text-left">
+                <img src="assets/images/books/book2.png" alt="21/40 Nights of Decrees and Your Enemies Will Surrender" class="item-img">
+                <div class="item-title">
+                    <h4 class=""><strong class="">21/40 Nights of Decrees and Your Enemies Will Surrender
+                        </strong></h4>
+                </div>
+                <div class="item-text">
+                    <p>This book is a collection of powerful prayer points divided in two parts. The first part is made up of 21 prayer topics or decrees and the second, 40 decrees. It covers every...</p>
+                </div>
+                <div class="item-button">
+                    <div class="row">
+                        <div class="col-12 mb-10" style="font-size: 10px; letter-spacing: 5px;text-transform: uppercase;font-weight: 500;">Buy Now</div>
+                        <div class="col-6" style="padding-left: 30px; padding-right: 5px;">
+                            <a href="#">
+                                <img src="assets/images/amazon.svg" width="100%" alt="">
+                            </a>
+                        </div>
+                        <div class="col-6" style="padding-left: 5px; padding-right: 30px;">
+                            <a href="#">
+                                <img src="assets/images/kindle.svg" width="100%" alt="">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="gallery-item col-md-3 dark shadow gallery-style-6 margin-x2 padding-x2 text-left">
+                <img src="assets/images/books/book1.png" alt="35 Special Dangerous Decrees" class="item-img">
+                <div class="item-title">
+                    <h4 class=""><strong class="">35 Special Dangerous Decrees</strong></h4>
+                </div>
+                <div class="item-text">
+                    <p class="">35 Special Dangerous Decrees is a collection of targeted prayers addressing 35 most important areas of the Christian life.</p>
+                </div>
+                <div class="item-button">
+                    <div class="row">
+                        <div class="col-12 mb-10" style="font-size: 10px; letter-spacing: 5px;text-transform: uppercase;font-weight: 500;">Buy Now</div>
+                        <div class="col-6" style="padding-left: 30px; padding-right: 5px;">
+                            <a href="#">
+                                <img src="assets/images/amazon.svg" width="100%" alt="">
+                            </a>
+                        </div>
+                        <div class="col-6" style="padding-left: 5px; padding-right: 30px;">
+                            <a href="#">
+                                <img src="assets/images/kindle.svg" width="100%" alt="">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bg-wrap">
+        <div class="bg" style="background-color: transparent;"></div>
+    </div>
+</section>
+<section id="bestsellers" class="sm pl-40 pr-40 pb-100 light full-height hidden-xl hidden-lg hidden-md" style="padding-top: 400px;">
+    <div class="container">
+        <div class="row align-items-center" style="display: flex;overflow-x: auto;flex-wrap: initial;">
+            <div id="title" style="white-space: nowrap;" class="align-self-center col-auto">
+                <img src="assets/images/bestseller.svg" width="90%" alt="">
+            </div>
+            <div class="col-5 dark gallery-item gallery-style-6 margin ml-50 padding shadow text-center">
+                <img src="assets/images/books/book2.png" alt="21/40 Nights of Decrees and Your Enemies Will Surrender" class="item-img">
+                <div class="item-title">
+                    <h4 class=""><strong class="">21/40 Nights of Decrees and Your Enemies Will Surrender
+                        </strong></h4>
+                </div>
+                <div class="item-text">
+                    <p>This book is a collection of powerful prayer points divided in two parts. The first part is made up of 21 prayer topics or decrees and the second, 40 decrees. It covers every...</p>
+                </div>
+                <div class="item-button">
+                    <div class="row">
+                        <div class="col-6" style="padding-left: 30px; padding-right: 5px;">
+                            <a href="#">
+                                <img src="assets/images/amazon.svg" width="100%" alt="">
+                            </a>
+                        </div>
+                        <div class="col-6" style="padding-left: 5px; padding-right: 30px;">
+                            <a href="#">
+                                <img src="assets/images/kindle.svg" width="100%" alt="">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-5 dark gallery-item gallery-style-6 margin ml-50 padding shadow text-center">
+                <img src="assets/images/books/book1.png" alt="35 Special Dangerous Decrees" class="item-img">
+                <div class="item-title">
+                    <h4 class=""><strong class="">35 Special Dangerous Decrees</strong></h4>
+                </div>
+                <div class="item-text">
+                    <p class="">35 Special Dangerous Decrees is a collection of targeted prayers addressing 35 most important areas of the Christian life.</p>
+                </div>
+                <div class="item-button">
+                    <div class="row">
+                        <div class="col-6" style="padding-left: 30px; padding-right: 5px;">
+                            <a href="#">
+                                <img src="assets/images/amazon.svg" width="100%" alt="">
+                            </a>
+                        </div>
+                        <div class="col-6" style="padding-left: 5px; padding-right: 30px;">
+                            <a href="#">
+                                <img src="assets/images/kindle.svg" width="100%" alt="">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-5 dark gallery-item gallery-style-6 margin ml-50 padding shadow text-center">
+                <img src="assets/images/books/book2.png" alt="21/40 Nights of Decrees and Your Enemies Will Surrender" class="item-img">
+                <div class="item-title">
+                    <h4 class=""><strong class="">21/40 Nights of Decrees and Your Enemies Will Surrender
+                        </strong></h4>
+                </div>
+                <div class="item-text">
+                    <p>This book is a collection of powerful prayer points divided in two parts. The first part is made up of 21 prayer topics or decrees and the second, 40 decrees. It covers every...</p>
+                </div>
+                <div class="item-button">
+                    <div class="row">
+                        <div class="col-6" style="padding-left: 30px; padding-right: 5px;">
+                            <a href="#">
+                                <img src="assets/images/amazon.svg" width="100%" alt="">
+                            </a>
+                        </div>
+                        <div class="col-6" style="padding-left: 5px; padding-right: 30px;">
+                            <a href="#">
+                                <img src="assets/images/kindle.svg" width="100%" alt="">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bg-wrap">
+        <div class="bg" style=""></div>
+    </div>
+</section>
+
+
+<?php
+    require_once 'inc/footer.php';
+?>
