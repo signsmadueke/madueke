3b037ee79b98c7579d8a172cd911f860c93df2fe
Signs Madueke <signsmadueke1@gmail.com>
2020-03-25 22:43:28 +0100

    ...

--- contact.php
+++ contact.php
@@ -4,30 +4,20 @@
     require_once 'inc/header.php';
 ?>
 
+<div id="space"></div>
 
-<section id="contacts" class="lg light full-height">
-    <div class="container">
-        <div class="align-items-center row">
-            <div>
-                <a href="mailto:hello@madueke.com">
-                    <h1>General</h1>
-                    <a href="mailto:hello@madueke.com">hello@madueke.com</a>
-                </a>
-            </div>
-            <div>
-                <a href="mailto:requests@madueke.com">
-                    <h1>Prayer Requests</h1>
-                    <a href="mailto:requests@madueke.com">requests@madueke.com</a>
-                </a>
-            </div>
-        </div>
-    </div>
-    <div class="bg-wrap">
-        <div class="bg lg hidden-sm"></div>
-        <div class="bg sm"></div>
-    </div>
+<section id="contacts">
+    <a href="mailto:hello@madueke.com">
+        <h1>General</h1>
+        <p>hello@madueke.com</p>
+    </a>
+    <a href="mailto:requests@madueke.com">
+        <h1>Prayer Requests</h1>
+        <p>requests@madueke.com</p>
+    </a>
 </section>
 
+<div id="space"></div>
 
 <?php
     require_once 'inc/footer.php';
