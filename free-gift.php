de55514bb1e7205d66388e9def43e0aa87a42c53
Signs Madueke <signsmadueke1@gmail.com>
2020-05-10 22:02:43 +0100

    Free Gift Page Added

---
+++ free-gift.php
@@ -0,0 +1,101 @@
+<?php
+    $title = 'Free Gift';
+    $page_description = 'Prayer M. Madueke, Christian Author of over 100 books on Amazon.';
+    $extraBodyClasses = '';
+    require_once 'inc/header.php';
+?>
+
+<section id="gift-title">
+    <div class="text">
+        <h1>Book Gift Pack</h1>
+        <p>These powerful books are sold on Amazon. But here, you get them for free.</p>
+    </div>
+</section>
+
+<section id="gifts">
+    <div class="gift">
+        <div class="image">
+            <img src="assets/images/books/book22.jpg" alt="Book Cover of Quarantined for God's Deliverance">
+        </div>
+        <div class="shadow">
+            <img src="assets/images/books/book22.jpg" alt="Book Cover of Quarantined for God's Deliverance">
+        </div>
+    </div>
+
+    <div class="gift">
+        <div class="image">
+            <img src="assets/images/books/book21.jpg" alt="Book Cover of Quarantined for God's Deliverance">
+        </div>
+        <div class="shadow">
+            <img src="assets/images/books/book21.jpg" alt="Book Cover of Quarantined for God's Deliverance">
+        </div>
+    </div>
+
+    <div class="gift">
+        <div class="image">
+            <img src="assets/images/books/book20.jpg" alt="Book Cover of Quarantined for God's Deliverance">
+        </div>
+        <div class="shadow">
+            <img src="assets/images/books/book20.jpg" alt="Book Cover of Quarantined for God's Deliverance">
+        </div>
+    </div>
+
+    <div class="gift">
+        <div class="image">
+            <img src="assets/images/books/book19.jpg" alt="Book Cover of Quarantined for God's Deliverance">
+        </div>
+        <div class="shadow">
+            <img src="assets/images/books/book19.jpg" alt="Book Cover of Quarantined for God's Deliverance">
+        </div>
+    </div>
+</section>
+
+<section id="gift-title">
+    <div class="text">
+        <h1>Where do I send the books?</h1>
+        <p>Fill the form below and you’ll get the download link in your email.<br>So make sure your email address is correct.</p>
+    </div>
+</section>
+
+<section id="gift-form">
+    <form action="https://prayermaduekestore.us15.list-manage.com/subscribe/post?u=a1ded7fdc64b5e8e70cbf9c53&amp;id=7956285b20" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+        <div class="form-inputs">
+            <div class="name">
+                <div class="mc-field-group">
+                    <input type="text" required value="" name="FNAME" class="" id="mce-FNAME" placeholder="eg: John">
+                    <label for="mce-FNAME">First Name</label>
+                </div>
+                <div class="mc-field-group">
+                    <input type="text" required value="" name="LNAME" class="" id="mce-LNAME" placeholder="eg: Doe">
+                    <label for="mce-LNAME">Last Name</label>
+                </div>
+            </div>
+
+            <div class="mc-field-group">
+                <input type="email" required value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="eg: johndoe@domain.com">
+                <label for="mce-EMAIL">Email Address</label>
+            </div>
+        </div>
+
+        <div class="form-bottom">
+            <input type="submit" value="Download Now" name="subscribe" id="mc-embedded-subscribe" class="button">
+
+            <div id="mce-responses" class="clear">
+                <div class="response" id="mce-error-response" style="display:none"></div>
+                <div class="response" id="mce-success-response" style="display:none"></div>
+            </div>
+
+            <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                <input type="text" name="b_a1ded7fdc64b5e8e70cbf9c53_7956285b20" tabindex="-1" value="">
+            </div>
+        </div>
+    </form>
+
+    <p class="disclaimer">We're committed to your privacy. We use the information you provide to us to contact you about our relevant content, devotionals, and new book releases. You may unsubscribe from these communications at any time.</p>
+    <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
+    <script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text'; }(jQuery));var $mcj = jQuery.noConflict(true);</script>
+</section>
+
+<?php
+    require_once 'inc/footer.php';
+?>
