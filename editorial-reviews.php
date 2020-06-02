a6275b5dd35688251163c3ec69569a4fa8f2cf32
Signs Madueke <signsmadueke1@gmail.com>
2020-06-02 22:50:41 +0100

    Editorial Reviews Page

---
+++ editorial-reviews.php
@@ -0,0 +1,63 @@
+<?php
+    $title = 'Free Gift';
+    $page_description = 'Get powerful books by Prayer M. Madueke for free.';
+    $page_image = "madueke/prayermadueke.jpg";
+    $extraBodyClasses = '';
+    require_once 'inc/header.php';
+?>
+
+<div" style="display: grid;grid-gap: 100px;grid-template-columns: repeat(auto-fit, minmax(300px, 300px));grid-auto-columns: max-content;grid-auto-rows: max-content;">
+    <div style="display: grid;grid-gap: 30px; grid-auto-rows: max-content;">
+        <img src="assets/images/reviewers/chris-akosa.jpg" alt="Chris Akosa" style="width: 100%; max-width: 100%; border-radius: 10px; box-shadow: 0px 12.4658px 25px rgba(0, 0, 0, 0.25);">
+        <div style="display: grid; grid-gap: 10px; width: 100%; max-width: 100%;">
+            <div style="display: grid; grid-gap: 10px;">
+                <h2 style="font-style: normal; font-weight: bold; font-size: 23px; line-height: 31px; font-feature-settings: 'pnum' on, 'lnum' on; color: #222222;">
+                    Chris Akosa
+                </h2>
+                <p style="font-style: normal; font-weight: 500;	font-size: 12px; line-height: 150%; color: #222222; opacity: 0.7;">
+                    General Superintendent, Abundant Life Global Outreach
+                </p>
+            </div>
+            <p style="font-style: normal; font-weight: 500; font-size: 13px; line-height: 23px;	color: #222222;	opacity: 0.9;">
+                Dangerous decrees to destroy your destroyers are a series of decrees that are designed to defend the defenceless and return evil arrows to their source. The decrees and prayers in these series of books stands as enemies to your enemies without taking no for an answer.
+            </p>
+        </div>
+    </div>
+    <div style="display: grid;grid-gap: 30px; grid-auto-rows: max-content;">
+        <img src="assets/images/reviewers/ken-uloh.jpg" alt="Ken Uloh" style="width: 100%; max-width: 100%; border-radius: 10px; box-shadow: 0px 12.4658px 25px rgba(0, 0, 0, 0.25);">
+        <div style="display: grid; grid-gap: 10px; width: 100%; max-width: 100%;">
+            <div style="display: grid; grid-gap: 10px;">
+                <h2 style="font-style: normal; font-weight: bold; font-size: 23px; line-height: 31px; font-feature-settings: 'pnum' on, 'lnum' on; color: #222222; width: 100%; max-width: 100%;">
+                    Ken Uloh
+                </h2>
+                <p style="font-style: normal; font-weight: 500;	font-size: 12px; line-height: 150%; color: #222222; opacity: 0.7; ma">
+                    Presiding Bishop, Divine Grace Liberation Assembly Worldwide
+                </p>
+            </div>
+            <p style="font-style: normal; font-weight: 500; font-size: 13px; line-height: 23px;	color: #222222;	opacity: 0.9;">
+                Dangerous decrees to destroy your destroyers are a series of decrees that are designed to defend the defenceless and return evil arrows to their source. The decrees and prayers in these series of books stands as enemies to your enemies without taking no for an answer.
+            </p>
+        </div>
+    </div>
+    <div style="display: grid;grid-gap: 30px; grid-auto-rows: max-content;">
+        <img src="assets/images/reviewers/robert-duru.jpg" alt="Robert Duru" style="width: 100%; max-width: 100%; border-radius: 10px; box-shadow: 0px 12.4658px 25px rgba(0, 0, 0, 0.25);">
+        <div style="display: grid; grid-gap: 10px; width: 100%; max-width: 100%;">
+            <div style="display: grid; grid-gap: 10px;">
+                <h2 style="font-style: normal; font-weight: bold; font-size: 23px; line-height: 31px; font-feature-settings: 'pnum' on, 'lnum' on; color: #222222;">
+                    Robert Duru
+                </h2>
+                <p style="font-style: normal; font-weight: 500;	font-size: 12px; line-height: 150%; color: #222222; opacity: 0.7; width: 100%; max-width: 100%;">
+                    General Overseer, JRC CHURCH
+                </p>
+            </div>
+            <p style="font-style: normal; font-weight: 500; font-size: 13px; line-height: 23px;	color: #222222;	opacity: 0.9;">
+                Dangerous decrees to destroy your destroyers are a series of decrees that are designed to defend the defenceless and return evil arrows to their source. The decrees and prayers in these series of books stands as enemies to your enemies without taking no for an answer.
+            </p>
+        </div>
+    </div>
+</div">
+
+
+<?php
+    require_once 'inc/footer.php';
+?>
