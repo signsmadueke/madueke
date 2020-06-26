dd6b1e358fb4c3f4b661a25399be8499294baef4
Signs Madueke <signsmadueke1@gmail.com>
2020-06-26 16:34:20 +0100

    Donation page

--- editorial-reviews.php
+++ editorial-reviews.php
@@ -1,3 +1,55 @@
+<style>td.apm-tablemodule-valuecell {
+	padding-top: 8px !important;
+	padding-left: 14px;
+	padding-bottom: 8px;
+	border-top: 1px solid #dddddd;
+	border-bottom: 1px solid #dddddd;
+	text-align: center;
+	width: inherit!important;
+}
+
+table.apm-tablemodule-table {
+	border-bottom: 1px solid #dddddd;
+	width: 100%;
+	left: 4%;
+	table-layout: fixed;
+	margin-bottom: 6px!important;
+}
+
+th.apm-tablemodule-image {
+	background-color: #FFFFFF;
+	padding: 0 !important;
+	text-align: center;
+	overflow: hidden;
+	vertical-align: bottom;
+}
+
+th.apm-tablemodule-blankkeyhead {
+	padding-bottom: 8px;
+	text-align: center;
+	border: 0 !important;
+	width: 18%;
+}
+
+th {}
+
+tr.apm-tablemodule-imagerows th {
+	padding-bottom: 8px;
+	text-align: center;
+}
+
+th.apm-tablemodule-keyhead {
+	border-top: 1px solid #dddddd;
+	padding: 8px 14px 6px !important;
+	background-color: #f7f7f7;
+	font-weight: normal;
+	color: #626262;
+	float: none !important;
+	margin-right: 0;
+	text-align: center;
+	width: 106px;
+}</style>
+
 <?php
     $title = 'Editorial Reviews';
     $page_description = 'Get powerful books by Prayer M. Madueke for free.';
@@ -57,6 +109,96 @@
     </div>
 </div">
 
+
+<table class="apm-tablemodule-table" cellspacing="0" cellpadding="0">
+    <tbody>
+        <tr class="apm-tablemodule-imagerows">
+            <th class="apm-tablemodule-blankkeyhead">
+            </th>
+            <th class="apm-tablemodule-image ">
+                <a href="/dp/1400203759/ref=emc_b_5_i">
+                    <img alt="love does" src="https://m.media-amazon.com/images/S/aplus-media/vc/ec5b58b0-4567-40c0-87f1-19e9d402a03d._CR0,0,150,300_PT0_SX150__.png" width="148.0px">
+                </a>
+            </th>
+            <th class="apm-tablemodule-image ">
+                <a href="/dp/0718078136/ref=emc_b_5_i">
+                    <img alt="everybody always" src="https://m.media-amazon.com/images/S/aplus-media/vc/d15a26ab-63b5-40e5-987a-818d5a1e47e5._CR0,0,150,300_PT0_SX150__.png" width="148.0px">
+                </a>
+            </th>
+            <th class="apm-tablemodule-image ">
+                <a href="/dp/0718095227/ref=emc_b_5_i">
+                    <img alt="love does for kids" src="https://m.media-amazon.com/images/S/aplus-media/vc/690a07c4-2319-456b-a88a-dbe9936629a9._CR0,0,150,300_PT0_SX150__.png" width="148.0px">
+                </a>
+            </th>
+            <th class="apm-tablemodule-image ">
+                <a href="/dp/1400203775/ref=emc_b_5_i">
+                    <img alt="Live in grace walk in love" src="https://m.media-amazon.com/images/S/aplus-media/vc/8b558783-d52e-4cd3-9f2e-7102039ed2e0.__CR0,0,150,300_PT0_SX150_V1___.png" width="148.0px">
+                </a>
+            </th>
+            <th class="apm-tablemodule-image ">
+                <a href="/dp/1400219493/ref=emc_b_5_i">
+                    <img alt="dream big bob goff" src="https://m.media-amazon.com/images/S/aplus-media/vc/45d29dfc-5172-4a3a-af34-c497df7c6558.__CR0,0,150,300_PT0_SX150_V1___.png" width="148.0px">
+                </a>
+            </th>
+        </tr>
+        <tr class="apm-tablemodule-imagerows">
+            <th class="apm-tablemodule-blankkeyhead">
+            </th>
+            <th> <a href="/dp/1400203759/ref=emc_b_5_t">
+                    Love Does
+                </a>
+            </th>
+            <th> <a href="/dp/0718078136/ref=emc_b_5_t">
+                    Everybody Always
+                </a>
+            </th>
+            <th> <a href="/dp/0718095227/ref=emc_b_5_t">
+                    Love Does for Kids
+                </a>
+            </th>
+            <th> <a href="/dp/1400203775/ref=emc_b_5_t">
+                    Live in Grace, Walk in Love
+                </a>
+            </th>
+            <th> <a href="/dp/1400219493/ref=emc_b_5_t">
+                    Dream Big
+                </a>
+            </th>
+        </tr>
+        <tr class="apm-tablemodule-keyvalue">
+            <th class="apm-tablemodule-keyhead">
+                <span class="a-color-secondary">
+                    Check out all of the books in Bob's library!
+                </span>
+            </th>
+            <td class="apm-tablemodule-valuecell" style="width:191px;">
+                <span class="a-size-base">
+                    The first book Bob Goff wrote, this whimsical and inspiring collection of stories was a runaway bestseller.
+                </span>
+            </td>
+            <td class="apm-tablemodule-valuecell" style="width:191px;">
+                <span class="a-size-base">
+                    The follow-up to Love Does (technically the third book, since the second was stolen out of his car), this book tackles how to love difficult people.
+                </span>
+            </td>
+            <td class="apm-tablemodule-valuecell" style="width:191px;">
+                <span class="a-size-base">
+                    Beautifully illustrated, this children's book takes the stories from Love Does and delivers them in a child-friendly manner.
+                </span>
+            </td>
+            <td class="apm-tablemodule-valuecell" style="width:191px;">
+                <span class="a-size-base">
+                    This 365-day journey will inspire you to step out in confidence, offer grace to yourself and others, and become love.
+                </span>
+            </td>
+            <td class="apm-tablemodule-valuecell" style="width:191px;">
+                <span class="a-size-base">
+                    Coming Soon! This is an inspiring and practical way to achieve your dreams! Available June 23rd, 2020!
+                </span>
+            </td>
+        </tr>
+    </tbody>
+</table>
 
 <?php
     require_once 'inc/footer.php';
