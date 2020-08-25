21b494b7f34920d058516c8d9658914ebb774e33
Signs Madueke <signsmadueke1@gmail.com>
2020-08-25 01:09:41 +0100

    Update free-gift.php

--- free-gift.php
+++ free-gift.php
@@ -10,13 +10,55 @@
 <html lang="en">
 
 <head>
-	<title></title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
-    <?php wp_head(); ?>
-    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+    <meta name="author" content="Prayer M. Madueke">
+    <meta name="description" content="<?= $page_description; ?>">
+    <meta name="keywords" 
+        content="Prayer, Madueke, Prayer M. Madueke, Christian Author, Christian, Author, Amazon, Religion, Spirituality, christianbook, devotion, faith, olukoya, prayer, prayer madueke, prayer rain, blessing prayer, prayer book, spiritual exercises."
+    >
+    <meta name="robots" content="index, follow">
+
+
+    <!-- Open Graph -->
+    <meta property="og:title" content="<?php echo $title; ?> — Prayer M. Madueke, Christian Author of over 100 books on Amazon">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="index">
+    <meta property="og:site_name" content="Prayer M. Madueke">
+    <meta property="og:image" content="assets/images/<?= $page_image; ?>">
+    <meta property="og:description" content="<?= $page_description; ?>">
+
+    <!-- Twitter Card data -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@prayermadueke">
+    <meta name="twitter:image:alt" content="<?php echo $title; ?> — Prayer M. Madueke, Christian Author of over 100 books on Amazon">
+    <meta name="twitter:creator" content="@prayermadueke">
+    <meta name="twitter:title" content="<?php echo $title; ?> — Prayer M. Madueke, Christian Author of over 100 books on Amazon">
+    <meta name="twitter:image" content="assets/images/<?= $page_image; ?>">
+    <meta name="twitter:description" content="<?= $page_description; ?>">
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="assets/css/styles.css"/>
+
+    <link rel="icon" href="favicon.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="favicon.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="favicon.png" />
+    <meta name="theme-color" content="#000000">
+    <title><?php echo $title . " — " . $tagline; ?></title>
+
+        
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131093422-2"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        
+        gtag('config', 'UA-131093422-2');
+    </script>
 </head>
 
-<body>
+<body class="container <?php echo $extraBodyClasses; ?>">
 
 <section id="boxset-hero">
     <div class="text animate reveal">
