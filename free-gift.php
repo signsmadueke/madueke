40fcf83eb9c8515426fe70f487399af26440143c
Signs Madueke <signsmadueke1@gmail.com>
2020-08-25 02:11:02 +0100

    Update free-gift.php

--- free-gift.php
+++ free-gift.php
@@ -254,7 +254,6 @@
 
     <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
     <script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text'; }(jQuery));var $mcj = jQuery.noConflict(true);</script>
-    <div class="cta animate reveal"><button data-open="download-boxset" class="button">Get boxset now!</button></div>
 </section>
 
 <div id="download-boxset" class="modal">
