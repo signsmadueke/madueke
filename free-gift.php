034eb93255ce2be2b912f6bd2130dac8bca5b14f
Signs Madueke <signsmadueke1@gmail.com>
2020-08-25 01:01:29 +0100

    Free gift page

--- free-gift.php
+++ free-gift.php
@@ -4,100 +4,259 @@
     $page_description = 'Get powerful books by Prayer M. Madueke for free.';
     $page_image = "madueke/prayermadueke.jpg";
     $extraBodyClasses = '';
-    require_once 'inc/header.php';
 ?>
 
-<section id="gift-title">
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+    <?php wp_head(); ?>
+    
+</head>
+
+<body>
+
+<section id="boxset-hero">
     <div class="text animate reveal">
-        <h1>Book Gift Pack</h1>
-        <p>These powerful books are sold on Amazon. But here, you get them for free.</p>
+        <h1>The Complete Life-Changing<br>Christian Bookset</h1>
+        <img class="svg decor" src="<?php bloginfo('template_directory');?>/assets/images/icons/decor.svg">
+    </div>
+
+    <div id="gifts">
+        <div class="gift animate reveal">
+            <div class="image">
+                <img src="<?php bloginfo('template_directory');?>/assets/images/books/book22.jpg" alt="Book Cover of Quarantined for God's Deliverance">
+            </div>
+            <div class="shadow">
+                <img src="<?php bloginfo('template_directory');?>/assets/images/books/book22.jpg" alt="Book Cover of Quarantined for God's Deliverance">
+            </div>
+        </div>
+
+        <div class="gift animate reveal">
+            <div class="image">
+                <img src="<?php bloginfo('template_directory');?>/assets/images/books/book21.jpg" alt="Book Cover of Quarantined for God's Deliverance">
+            </div>
+            <div class="shadow">
+                <img src="<?php bloginfo('template_directory');?>/assets/images/books/book21.jpg" alt="Book Cover of Quarantined for God's Deliverance">
+            </div>
+        </div>
+
+        <div class="gift animate reveal">
+            <div class="image">
+                <img src="<?php bloginfo('template_directory');?>/assets/images/books/book19.jpg" alt="Book Cover of Quarantined for God's Deliverance">
+            </div>
+            <div class="shadow">
+                <img src="<?php bloginfo('template_directory');?>/assets/images/books/book19.jpg" alt="Book Cover of Quarantined for God's Deliverance">
+            </div>
+        </div>
+
+        <div class="gift animate reveal">
+            <div class="image">
+                <img src="<?php bloginfo('template_directory');?>/assets/images/books/book20.jpg" alt="Book Cover of Quarantined for God's Deliverance">
+            </div>
+            <div class="shadow">
+                <img src="<?php bloginfo('template_directory');?>/assets/images/books/book20.jpg" alt="Book Cover of Quarantined for God's Deliverance">
+            </div>
+        </div>
     </div>
 </section>
 
-<section id="gifts">
-    <div class="gift animate reveal">
-        <div class="image">
-            <img src="assets/images/books/book22.jpg" alt="Book Cover of Quarantined for God's Deliverance">
-        </div>
-        <div class="shadow">
-            <img src="assets/images/books/book22.jpg" alt="Book Cover of Quarantined for God's Deliverance">
-        </div>
-    </div>
-
-    <div class="gift animate reveal">
-        <div class="image">
-            <img src="assets/images/books/book21.jpg" alt="Book Cover of Quarantined for God's Deliverance">
-        </div>
-        <div class="shadow">
-            <img src="assets/images/books/book21.jpg" alt="Book Cover of Quarantined for God's Deliverance">
-        </div>
-    </div>
-
-    <div class="gift animate reveal">
-        <div class="image">
-            <img src="assets/images/books/book20.jpg" alt="Book Cover of Quarantined for God's Deliverance">
-        </div>
-        <div class="shadow">
-            <img src="assets/images/books/book20.jpg" alt="Book Cover of Quarantined for God's Deliverance">
-        </div>
-    </div>
-
-    <div class="gift animate reveal">
-        <div class="image">
-            <img src="assets/images/books/book19.jpg" alt="Book Cover of Quarantined for God's Deliverance">
-        </div>
-        <div class="shadow">
-            <img src="assets/images/books/book19.jpg" alt="Book Cover of Quarantined for God's Deliverance">
-        </div>
-    </div>
+<section id="sticky-cta">
+    <h3 class="">Get this Life-Changing Boxset today!</h3>
+    <button data-open="download-boxset" class="button cta">Get boxset now!</button>
 </section>
 
-<section id="gift-title">
+<section id="about-author">
     <div class="text animate reveal">
-        <h1>Where do I send the books?</h1>
-        <p>Fill the form below and you’ll get the download link in your email.<br>So make sure your email address is correct.</p>
+        <h1>About the Author</h1>
+        <img class="svg decor" src="<?php bloginfo('template_directory');?>/assets/images/icons/decor.svg">
+    </div>
+
+    <div class="about">
+        <img class="" src="<?php bloginfo('template_directory');?>/assets/images/madueke/madueke2.jpg">
+        <div class="content">
+            <h3>Prayer M. Madueke</h3>
+            <p>Prayer Madueke is a Spiritual Warrior in the Lord’s Vineyard and founder of Prayer Emancipation Missions. An accomplished author, speaker, and expert on spiritual warfare and deliverance, he has published well over 100 books on every area of successful Christian living.<br><br>He is an acclaimed family and relationship counselor with several titles dealing with those critical areas in the lives of the children of God. He is blessed with a beautiful wife and 5 God-fearing children.<br><br>He travels to several countries each year speaking and conducting deliverance, breaking the yokes of demonic oppression, and setting captives free.</p>
+            <div id="social">
+                <ul>
+                    <li>
+                        <a href="https://facebook.com/prayermadueke">
+                            <img class="svg" src="<?php bloginfo('template_directory');?>/assets/images/icons/social/facebook.svg" height="15px" alt="Facebook">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://instagram.com/prayermadueke">
+                            <img class="svg" src="<?php bloginfo('template_directory');?>/assets/images/icons/social/instagram.svg" height="15px" alt="Instagram">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com/prayermadueke">
+                            <img class="svg" src="<?php bloginfo('template_directory');?>/assets/images/icons/social/twitter.svg" height="15px" alt="Twitter">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </section>
 
-<section id="gift-form" class="animate reveal">
-    <form action="https://prayermaduekestore.us15.list-manage.com/subscribe/post?u=a1ded7fdc64b5e8e70cbf9c53&amp;id=7956285b20" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-        <div class="form-inputs">
+<section id="editorial-reviews">
+    <div class="text animate reveal">
+        <h1>Editorial Reviews</h1>
+        <img class="svg decor" src="<?php bloginfo('template_directory');?>/assets/images/icons/decor.svg">
+    </div>
+
+    <div id="reviews">
+        <div class="review animate reveal">
+            <img class="picture" src="<?php bloginfo('template_directory');?>/assets/images/reviewers/chris-akosa.jpg">
             <div class="name">
-                <div class="mc-field-group">
-                    <input type="text" required value="" name="FNAME" class="" id="mce-FNAME" placeholder="eg: John">
-                    <label for="mce-FNAME">First Name</label>
-                </div>
-                <div class="mc-field-group">
-                    <input type="text" required value="" name="LNAME" class="" id="mce-LNAME" placeholder="eg: Doe">
-                    <label for="mce-LNAME">Last Name</label>
-                </div>
+                <h6>Rev. Chris Akosa</h6>
+                <p>Founder, Abundant Life Global Outreach</p>
             </div>
-
-            <div class="mc-field-group">
-                <input type="email" required value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="eg: johndoe@domain.com">
-                <label for="mce-EMAIL">Email Address</label>
-            </div>
+            <img class="svg decor" src="<?php bloginfo('template_directory');?>/assets/images/icons/decor.svg">
+            <p class="comment">Great books from an amazing teacher. Prayer Madueke really brings to life the power we release into our lives and the world when we partner with the Creator to speak forth His word. So good! So empowering.</p>
         </div>
 
-        <div class="form-bottom">
-            <input type="submit" value="Download Now" name="subscribe" id="mc-embedded-subscribe" class="button">
-
-            <div id="mce-responses" class="clear">
-                <div class="response" id="mce-error-response" style="display:none"></div>
-                <div class="response" id="mce-success-response" style="display:none"></div>
+        <div class="review animate reveal">
+            <img class="picture" src="<?php bloginfo('template_directory');?>/assets/images/reviewers/ken-uloh.jpg">
+            <div class="name">
+                <h6>Bishop Ken Dominics Uloh</h6>
+                <p>Bishop, Divine Grace Liberation Assembly Worldwide</p>
             </div>
-
-            <div style="position: absolute; left: -5000px;" aria-hidden="true">
-                <input type="text" name="b_a1ded7fdc64b5e8e70cbf9c53_7956285b20" tabindex="-1" value="">
-            </div>
+            <img class="svg decor" src="<?php bloginfo('template_directory');?>/assets/images/icons/decor.svg">
+            <p class="comment">These magnificent books by Prayer Madueke, will empower believers with the tools to boldly release decrees that will unlock the blessings of heaven, align hearts with the Word of God, and bring change to their lives. Bravo!</p>
         </div>
-    </form>
 
-    <p class="disclaimer">We're committed to your privacy. We use the information you provide to us to contact you about our relevant content, devotionals, and new book releases. You may unsubscribe from these communications at any time.</p>
-    <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
-    <script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text'; }(jQuery));var $mcj = jQuery.noConflict(true);</script>
+        <div class="review animate reveal">
+            <img class="picture" src="<?php bloginfo('template_directory');?>/assets/images/reviewers/okey-mofunanya.jpg">
+            <div class="name">
+                <h6>Pastor Okey Mofunanya</h6>
+                <p>SATGO in charge of Security, RCCG Worldwide</p>
+            </div>
+            <img class="svg decor" src="<?php bloginfo('template_directory');?>/assets/images/icons/decor.svg">
+            <p class="comment">Prayer Madueke has found the keys to releasing the blessing of the Father through the spoken Word to create supernatural breakthroughs in the name of Jesus and by the power of the Spirit. Get ready for the most dramatic answers to prayer ever!</p>
+        </div>
+
+        <div class="review animate reveal">
+            <img class="picture" src="<?php bloginfo('template_directory');?>/assets/images/reviewers/robert-duru.jpg">
+            <div class="name">
+                <h6>Pastor Robert Duru</h6>
+                <p>General Overseer, JRC CHURCH</p>
+            </div>
+            <img class="svg decor" src="<?php bloginfo('template_directory');?>/assets/images/icons/decor.svg">
+            <p class="comment">Powerful books for those wishing to draw nearer to God and to see their lives transformed. I highly recommend these resources as you will never be the same again. Five stars for an excellently written book.</p>
+        </div>
+    </div>
 </section>
 
-<?php
-    require_once 'inc/footer.php';
-?>
+<section id="amazon-reviews">
+    <div class="text animate reveal">
+        <h1>What my readers on Amazon are saying</h1>
+        <img class="svg decor" src="<?php bloginfo('template_directory');?>/assets/images/icons/decor.svg">
+    </div>
+
+    <div id="reviews">
+        <img class="review animate reveal" src="<?php bloginfo('template_directory');?>/assets/images/reviews/1.png">
+
+        <img class="review animate reveal" src="<?php bloginfo('template_directory');?>/assets/images/reviews/2.png">
+
+        <img class="review animate reveal" src="<?php bloginfo('template_directory');?>/assets/images/reviews/3.png">
+
+        <img class="review animate reveal" src="<?php bloginfo('template_directory');?>/assets/images/reviews/4.png">
+    </div>
+</section>
+
+<section id="boxset-cta">
+    <h1 class="animate reveal">Get this Life-Changing<br>Bookset today!</h1>
+    <div class="cta animate reveal"><button data-open="download-boxset" class="button">Get boxset now!</button></div>
+</section>
+
+<div id="download-boxset" class="modal">
+    <div class="modal-dialog">
+        <header class="modal-header">
+            <button title="Close" data-close class="close button">✕</button>
+        </header>
+        <div class="modal-hero">
+            <img class="svg decor" src="<?php bloginfo('template_directory');?>/assets/images/books/boxset1.png">
+        </div>
+        <div class="form modal-content">
+            <div class="form-use">
+                <h1>Tell me where to send them</h1>
+            </div>
+
+            <form action="https://prayermaduekestore.us15.list-manage.com/subscribe/post?u=a1ded7fdc64b5e8e70cbf9c53&amp;id=7956285b20" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                <div class="form-inputs">
+                    <div class="name">
+                        <div class="mc-field-group">
+                            <input type="text" required value="" name="FNAME" class="" id="mce-FNAME" placeholder="eg: John">
+                            <label for="mce-FNAME">First Name</label>
+                        </div>
+                        <div class="mc-field-group">
+                            <input type="text" required value="" name="LNAME" class="" id="mce-LNAME" placeholder="eg: Doe">
+                            <label for="mce-LNAME">Last Name</label>
+                        </div>
+                    </div>
+
+                    <div class="mc-field-group">
+                        <input type="email" required value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="eg: johndoe@domain.com">
+                        <label for="mce-EMAIL">Email Address</label>
+                    </div>
+                </div>
+
+                <div class="form-bottom">
+                    <input type="submit" value="Send my free boxset!" name="subscribe" id="mc-embedded-subscribe" class="button">
+                    <p class="disclaimer">We're committed to your privacy. We will not spam, rent or sell your information. We use the information you provide to us to contact you about our relevant content, devotionals, and new book releases. You may unsubscribe from these communications at any time.</p>
+
+                    <div id="mce-responses" class="clear">
+                        <div class="response" id="mce-error-response" style="display:none"></div>
+                        <div class="response" id="mce-success-response" style="display:none"></div>
+                    </div>
+
+                    <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                        <input type="text" name="b_a1ded7fdc64b5e8e70cbf9c53_7956285b20" tabindex="-1" value="">
+                    </div>
+                </div>
+            </form>
+
+            <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
+            <script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text'; }(jQuery));var $mcj = jQuery.noConflict(true);</script>
+        </div>
+    </div>
+</div>
+
+
+<footer id="footer" class="container" style="padding: 30px; grid-template-columns: 1fr 1fr;">
+    <p class="copyright">&copy; 2020 Prayer Madueke. All rights reserved.</p>
+    <div id="social">
+        <p>Connect with me</p>
+        <span></span>
+        <ul>
+			<li>
+                <a href="https://facebook.com/prayermadueke">
+                    <img class="svg" src="<?php bloginfo('template_directory');?>/assets/images/icons/social/facebook.svg'; ?>" height="15px" alt="Facebook">
+                </a>
+            </li>
+            <li>
+                <a href="https://instagram.com/prayermadueke">
+                    <img class="svg" src="<?php bloginfo('template_directory');?>/assets/images/icons/social/instagram.svg'; ?>" height="15px" alt="Instagram">
+                </a>
+            </li>
+            <li>
+                <a href="https://twitter.com/prayermadueke">
+                    <img class="svg" src="<?php bloginfo('template_directory');?>/assets/images/icons/social/twitter.svg'; ?>" height="15px" alt="Twitter">
+                </a>
+            </li>
+        </ul>
+    </div>
+</footer>
+
+<script src="assets/js/popper.js"></script>
+<script src="assets/js/jquery.js"></script>
+<script src="assets/js/custom.js"></script>
+<script src="assets/js/smooth-scroll.js"></script>
+<script src="assets/js/animations.js"></script>
+</body>
+
+</html>
