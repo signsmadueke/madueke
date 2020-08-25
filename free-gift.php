dda86004307f1ce329aa23df80d9accb129f08ae
Signs Madueke <signsmadueke1@gmail.com>
2020-08-25 02:18:23 +0100

    Update free-gift.php

--- free-gift.php
+++ free-gift.php
@@ -318,17 +318,17 @@
         <ul>
 			<li>
                 <a href="https://facebook.com/prayermadueke">
-                    <img class="svg" src="assets/images/icons/social/facebook.svg';?>" height="15px" alt="Facebook">
+                    <img class="svg" src="assets/images/icons/social/facebook.svg" height="15px" alt="Facebook">
                 </a>
             </li>
             <li>
                 <a href="https://instagram.com/prayermadueke">
-                    <img class="svg" src="assets/images/icons/social/instagram.svg';?>" height="15px" alt="Instagram">
+                    <img class="svg" src="assets/images/icons/social/instagram.svg" height="15px" alt="Instagram">
                 </a>
             </li>
             <li>
                 <a href="https://twitter.com/prayermadueke">
-                    <img class="svg" src="assets/images/icons/social/twitter.svg';?>" height="15px" alt="Twitter">
+                    <img class="svg" src="assets/images/icons/social/twitter.svg" height="15px" alt="Twitter">
                 </a>
             </li>
         </ul>
