73dd2ee3de5360ab5d5dded5e8f82075901e6c6b
godwintrav <godwintrav@gmail.com>
2021-07-08 09:05:58 +0100

    ...

--- inc/footer.php
+++ inc/footer.php
@@ -22,6 +22,31 @@
         </ul>
     </div>
 </footer>
+    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131093422-2"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        
+        gtag('config', 'UA-131093422-2');
+    </script>
+    <!-- Facebook Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1957151911105775');
+        fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"src="https://www.facebook.com/tr?id=1957151911105775&ev=PageView&noscript=1"/></noscript>
+    <!-- End Facebook Pixel Code -->
 <script src="assets/js/popper.js"></script>
 <script src="assets/js/jquery.js"></script>
 <script src="assets/js/custom.js"></script>
