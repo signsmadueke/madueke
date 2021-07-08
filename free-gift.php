41e800a43b03ebe71072b88aae3a16094771b769
godwintrav <godwintrav@gmail.com>
2021-07-09 00:08:26 +0100

    Free gift page

--- free-gift.php
+++ free-gift.php
@@ -120,22 +120,17 @@
         </div>
     </div>
 
-    <div class="cta animate reveal"><a href="#boxset-cta" class="button">Get bookset now!</a></div>
+    <!-- <div class="cta animate reveal"><a href="#boxset-cta" class="button">Get bookset now!</a></div> -->
 </section>
 
-<!-- <section id="sticky-cta">
-    <h3 class="">Get this Life-Changing Bookset today!</h3>
-    <button data-open="download-boxset" class="button cta">Get bookset now!</button>
-</section> -->
-
 <section id="about-author">
-    <div class="text animate reveal">
-        <h1 class="animate reveal">About the Author</h1>
-        <img class="svg decor animate reveal" src="assets/images/icons/decor.svg">
+<div class="text animate reveal">
+        <h1>About the Author</h1>
+        <img class="svg decor" src="assets/images/icons/decor.svg">
     </div>
 
     <div class="about">
-        <img class="animate reveal" src="assets/images/madueke/madueke2.jpg">
+        <img class="author-image animate reveal" src="assets/images/madueke/madueke3.jpg">
         <div class="content">
             <h3 class="animate reveal">Prayer M. Madueke</h3>
             <p class="animate reveal">Prayer Madueke is a Spiritual Warrior in the Lord’s Vineyard and founder of Prayer Emancipation Missions. An accomplished author, speaker, and expert on spiritual warfare and deliverance, he has published well over 100 books on every area of successful Christian living.<br><br>He is an acclaimed family and relationship counselor with several titles dealing with those critical areas in the lives of the children of God. He is blessed with a beautiful wife and 5 God-fearing children.<br><br>He travels to several countries each year speaking and conducting deliverance, breaking the yokes of demonic oppression, and setting captives free.</p>
@@ -209,6 +204,13 @@
             <p class="comment">Powerful books for those wishing to draw nearer to God and to see their lives transformed. I highly recommend these resources as you will never be the same again. Five stars for an excellently written book.</p>
         </div>
     </div>
+
+    <!-- <div class="cta animate reveal"><a href="#boxset-cta" class="button">Get book now!</a></div> -->
+</section>
+
+<section id="sticky-cta">
+    <h3 class="">Get this Life-Changing Bookset today!</h3>
+    <a href="#boxset-cta" class="button">Get book now!</a>
 </section>
 
 <section id="amazon-reviews">
@@ -238,18 +240,14 @@
         <div class="form-inputs">
             <div class="name">
                 <div class="mc-field-group animate reveal">
-                    <input type="text" required value="" name="FNAME" class="" id="mce-FNAME" placeholder="eg: John">
+                    <input type="text" autocomplete="on" required value="" name="FNAME" class="" id="mce-FNAME" placeholder="eg: John">
                     <label for="mce-FNAME">First Name</label>
                 </div>
-                <div class="mc-field-group animate reveal">
-                    <input type="text" required value="" name="LNAME" class="" id="mce-LNAME" placeholder="eg: Doe">
-                    <label for="mce-LNAME">Last Name</label>
-                </div>
-            </div>
 
-            <div class="mc-field-group animate reveal">
-                <input type="email" required value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="eg: johndoe@domain.com">
-                <label for="mce-EMAIL">Email Address</label>
+                <div class="mc-field-group animate reveal">
+                    <input type="email" autocomplete="on" required value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="eg: johndoe@domain.com">
+                    <label for="mce-EMAIL">Email Address</label>
+                </div>
             </div>
         </div>
 
@@ -271,60 +269,6 @@
     <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
     <script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
 </section>
-
-<div id="download-boxset" class="modal">
-    <div class="modal-dialog">
-        <header class="modal-header">
-            <button title="Close" data-close class="close button">✕</button>
-        </header>
-        <div class="modal-hero">
-            <img class="svg decor" src="assets/images/books/boxset1.png">
-        </div>
-        <div class="form modal-content">
-            <div class="form-use">
-                <h1>Tell me where to send them</h1>
-            </div>
-
-            <form action="https://prayermaduekestore.us15.list-manage.com/subscribe/post?u=a1ded7fdc64b5e8e70cbf9c53&amp;id=7956285b20" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-                <div class="form-inputs">
-                    <div class="name">
-                        <div class="mc-field-group">
-                            <input type="text" required value="" name="FNAME" class="" id="mce-FNAME" placeholder="eg: John">
-                            <label for="mce-FNAME">First Name</label>
-                        </div>
-                        <div class="mc-field-group">
-                            <input type="text" required value="" name="LNAME" class="" id="mce-LNAME" placeholder="eg: Doe">
-                            <label for="mce-LNAME">Last Name</label>
-                        </div>
-                    </div>
-
-                    <div class="mc-field-group">
-                        <input type="email" required value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="eg: johndoe@domain.com">
-                        <label for="mce-EMAIL">Email Address</label>
-                    </div>
-                </div>
-
-                <div class="form-bottom">
-                    <input type="submit" value="Send my free bookset!" name="subscribe" id="mc-embedded-subscribe" class="button">
-                    <p class="disclaimer">We're committed to your privacy. We will not spam, rent or sell your information. We use the information you provide to us to contact you about our relevant content, devotionals, and new book releases. You may unsubscribe from these communications at any time.</p>
-
-                    <div id="mce-responses" class="clear">
-                        <div class="response" id="mce-error-response" style="display:none"></div>
-                        <div class="response" id="mce-success-response" style="display:none"></div>
-                    </div>
-
-                    <div style="position: absolute; left: -5000px;" aria-hidden="true">
-                        <input type="text" name="b_a1ded7fdc64b5e8e70cbf9c53_7956285b20" tabindex="-1" value="">
-                    </div>
-                </div>
-            </form>
-
-            <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
-            <script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text'; }(jQuery));var $mcj = jQuery.noConflict(true);</script>
-        </div>
-    </div>
-</div>
-
 
 <footer id="footer" class="container" style="padding: 30px; grid-template-columns: 1fr 1fr;">
     <p class="copyright">&copy; 2020 Prayer Madueke. All rights reserved.</p>
