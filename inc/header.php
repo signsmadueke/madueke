28c4e433bb203fb510e4a1618b44573cbcdd0200
godwintrav <godwintrav@gmail.com>
2021-07-08 08:59:04 +0100

    fb from head to foot

--- inc/header.php
+++ inc/header.php
@@ -38,33 +38,6 @@
     <link rel="apple-touch-icon" sizes="120x120" href="favicon.png" />
     <link rel="apple-touch-icon" sizes="152x152" href="favicon.png" />
     <meta name="theme-color" content="#000000">
-
-        
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131093422-2"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        
-        gtag('config', 'UA-131093422-2');
-    </script>
-    <!-- Facebook Pixel Code -->
-    <script>
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '1957151911105775');
-        fbq('track', 'PageView');
-    </script>
-    <noscript><img height="1" width="1" style="display:none"src="https://www.facebook.com/tr?id=1957151911105775&ev=PageView&noscript=1"/></noscript>
-    <!-- End Facebook Pixel Code -->
-
 </head>
 
 <body class="container <?php echo $extraBodyClasses; ?>">
